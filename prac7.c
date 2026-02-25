74c4411684851f3280fbd8378ecb1a8ba2a4e643
MizxGaming <niharbalanayakdudu@gmail.com>
2026-02-25 20:59:09 +0530

    19/02/2026

--- prac7.c
+++ prac7.c
@@ -4,9 +4,13 @@
 int main()
 {
     char c;
+
     clrscr();
+
     printf("Enter the character: ");
     scanf("%c", &c);
+
     printf("The ASCII code of the character is: %d", c);
+
     return 0;
 }
