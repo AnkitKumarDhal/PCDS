6505cd598fb7c2cd14f1ddea56e68b75ef9990a4
MizxGaming <niharbalanayakdudu@gmail.com>
2026-04-08 23:21:26 +0530

    updated the quadratic roots

--- Prac-2/prac11.c
+++ Prac-2/prac11.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    float a, b, c, D, r1, r2;
+    float a, b, c, D, r1, r2, r_p, r_i;
     clrscr();
 
     printf("Enter the coefficient of x^2, x and the constant: ");
@@ -15,13 +15,24 @@ int main()
     {
         r1 = ((-b) + sqrt(D)) / (2 * a);
         r2 = ((-b) - sqrt(D)) / (2 * a);
-
-        printf("The roots of the quadratic equation (%f)x^2 + (%f)x + (%f) are: \n", a, b, c);
-        printf("%f and %f\n", r1, r2);
+        printf("The roots are real and distinct.\n");
+        printf("Root 1: %f\n", r1);
+        printf("Root 2: %f\n", r2);
+    }
+    else if (D == 0)
+    {
+        r1 = (-b) / (2 * a);
+        r2 = r1;
+        printf("The Roots are real and equal.\n");
+        printf("The roots are: %f\n", r1);
     }
     else
     {
-        printf("The quadratic equation has imaginary roots.\n");
+        r_p = (-b) / (2 * a);
+        r_i = sqrt(-D) / (2 * a);
+        printf("The roots of the equation are imaginary.\n");
+        printf("Root 1: %f + i%f\n", r_p, r_i);
+        printf("Root 2: %f - i%f\n", r_p, r_i);
     }
 
     return 0;
