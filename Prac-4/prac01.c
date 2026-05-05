97c6c17e315ab3fea2d2170e4847f20d853bff80
MizxGaming <niharbalanayakdudu@gmail.com>
2026-05-05 20:17:37 +0530

    added first question of 4th set

---
+++ Prac-4/prac01.c
@@ -0,0 +1,23 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    int a[50], n, i;
+    clrscr();
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Printing the elements: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+    }
+}
