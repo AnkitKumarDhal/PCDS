559bbe5663e1d83e5e181e39596b35d2e32dd7c4
MizxGaming <niharbalanayakdudu@gmail.com>
2026-05-14 08:58:11 +0530

    optimized fibonacci using recursion code

--- Prac-6/prac06.c
+++ Prac-6/prac06.c
@@ -3,13 +3,9 @@
 
 int fibonacci(int n)
 {
-    if (n == 0)
+    if (n <= 1)
     {
-        return 0;
-    }
-    else if (n == 1)
-    {
-        return 1;
+        return n;
     }
     else
     {
